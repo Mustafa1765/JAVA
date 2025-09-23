4b7675a5d40dbd1461585667500fac03078a07d4
Mustafa1765 <shaikmustafaali1765@gmail.com>
2025-09-23 20:21:46 +0530

    java program

---
+++ TestPolymorphism.java
@@ -0,0 +1,27 @@
+
+class Animal {
+       void sound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+class Dog extends Animal {
+      void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+class Cat extends Animal {
+       void sound() {
+        System.out.println("Cat meows");
+    }
+}
+
+ public class TestPolymorphism {  
+    public static void main(String[] args) {
+               Animal animal1 = new Dog();
+        animal1.sound();  
+                Animal animal2 = new Cat();
+        animal2.sound(); 
+    }
+}
