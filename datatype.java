a8f3323c9612b6d75ba5060b0cc2532e258557be
harishdev17 <150819844+harishdev17@users.noreply.github.com>
2025-09-29 20:00:04 +0530

    Update datatype.java

--- datatype.java
+++ datatype.java
@@ -11,6 +11,13 @@ class defaultdemo
  public static void main(String[] args) 
  { 
  System.out.println("The default values of primitive data types are:");
-
+  System.out.println("Byte : " + b);
+  System.out.println("Short : " + s);
+  System.out.println("Int : " + i);
+  System.out.println("Long : " + l);
+  System.out.println("Float : " + f);
+  System.out.println("Double : " + d);
+  System.out.println("Char : " + c);   
+  System.out.println("Boolean : " + bl);
  }
 }
