dfe0484d5c8ffabf5cc60b67652c0d2dff9c62f0
Mustafa1765 <shaikmustafaali1765@gmail.com>
2025-09-29 11:23:36 +0530

    Create datatype.java

---
+++ datatype.java
@@ -0,0 +1,16 @@
+class defaultdemo
+{
+ static byte b;
+ static short s;
+ static int i;
+ static long l;
+ static float f;
+ static double d;
+ static char c;
+ static boolean bl;
+ public static void main(String[] args) 
+ { 
+ System.out.println("The default values of primitive data types are:");
+
+ }
+}
